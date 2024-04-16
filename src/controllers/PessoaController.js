846b57f56d8b2189ebf7bc2471d59f8b2400b2f0
rodrigocloureiro <rodrigocostaloureiro563@gmail.com>
2024-04-16 19:29:36 -0300

    PessoaController criada

---
+++ src/controllers/PessoaController.js
@@ -0,0 +1,11 @@
+class PessoaController {
+  static async pegaTodas(req, res) {
+    try {
+      // acessar o modelo
+    } catch (erro) {
+      // tratamento erro
+    }
+  }
+}
+
+module.exports = PessoaController;
