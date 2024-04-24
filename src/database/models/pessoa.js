65fe6ccb71a3c9bd57c26ca090b8524c3c7fd432
rodrigocloureiro <rodrigocostaloureiro563@gmail.com>
2024-04-24 11:59:55 -0300

    add: escopo padrão criado para retornar apenas pessoas ATIVAS

--- src/database/models/pessoa.js
+++ src/database/models/pessoa.js
@@ -32,6 +32,11 @@ module.exports = (sequelize, DataTypes) => {
     modelName: 'Pessoa',
     tableName: 'pessoas',
     paranoid: true,
+    defaultScope: {
+      where: {
+        ativo: true,
+      },
+    },
   });
   return Pessoa;
 };
